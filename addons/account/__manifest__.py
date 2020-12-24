1f31fb6a57a9996cd88b9bb068b8e27fdea9c184
azhar007elhar <azhar7elhar@gmail.com>
2020-12-24 10:42:54 +0100

    commit

--- addons/account/__manifest__.py
+++ addons/account/__manifest__.py
@@ -19,9 +19,9 @@ You could use this simplified accounting in case you work with an (external) acc
     'data': [
         'security/account_security.xml',
         'security/ir.model.access.csv',
-        'data/data_account_type.xml',
-        'data/account_data.xml',
-        'data/digest_data.xml',
+        # 'data/data_account_type.xml',
+        # 'data/account_data.xml',
+        # 'data/digest_data.xml',
         'views/account_menuitem.xml',
         'views/account_payment_view.xml',
         'wizard/account_accrual_accounting_view.xml',
